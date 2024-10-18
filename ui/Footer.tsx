b8591f96fd1126cc559f21af221d532f06e94640
Faraz <63188075+FarazAhmad541@users.noreply.github.com>
2024-10-18 11:27:06 +0500

    adjusted footer

--- ui/Footer.tsx
+++ ui/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <div className='w-full justify-self-end py-5 text-secondaryLight border-t-secondaryLight border-t-[1px] flex justify-center items-center'>
-      <div className='md:max-w-[800px] w-full px-3 cursor-not-allowed'>
-        <p>Made By Faraz</p>
+      <div className='md:max-w-[800px] w-full px-7 md:px-3 cursor-not-allowed'>
+        <p className='text-sm md:text-base lg:text-base'>Made By Faraz</p>
       </div>
     </div>
   )
