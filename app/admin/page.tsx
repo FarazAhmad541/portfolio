bfa5e6898a631b087b64b8bf569c6ccb0acd944c
Faraz <63188075+FarazAhmad541@users.noreply.github.com>
2024-10-19 16:24:37 +0500

    fixed bugs

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,8 @@
 import MdxEditor from './MdxEditor'
 export default function Page() {
-  const formAction = async (article) => {
-    'use server'
-    console.log(article)
-  }
   return (
     <div className='flex-grow'>
-      <MdxEditor formAction={formAction} />
+      <MdxEditor />
     </div>
   )
 }
