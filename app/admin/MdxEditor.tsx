8f7ea2d20bf388590a55aff20cbb726f22fa5c57
Faraz <63188075+FarazAhmad541@users.noreply.github.com>
2024-10-19 10:45:23 +0500

    fixed editor errors

--- app/admin/MdxEditor.tsx
+++ app/admin/MdxEditor.tsx
@@ -1,19 +1,19 @@
-"use client";
-import "@uiw/react-md-editor/markdown-editor.css";
-import "@uiw/react-markdown-preview/markdown.css";
-import dynamic from "next/dynamic";
-import { useState } from "react";
-import ArticleForm from "./ArticleForm";
+'use client'
+import '@uiw/react-markdown-preview/markdown.css'
+import '@uiw/react-md-editor/markdown-editor.css'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import ArticleForm from './ArticleForm'
 
-const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
+const MDEditor = dynamic(() => import('@uiw/react-md-editor'), { ssr: false })
 export default function MdxEditor() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('')
 
   return (
-    <div className="max-w-4xl">
-      <MDEditor value={value} onChange={setValue} />
+    <div className='max-w-4xl'>
+      <MDEditor value={value} onChange={(value) => setValue(value || '')} />
 
       <ArticleForm />
     </div>
-  );
+  )
 }
