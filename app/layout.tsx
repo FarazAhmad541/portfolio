55585f9126b2c7cc47a4da2e8b522457ac1067e8
Faraz <63188075+FarazAhmad541@users.noreply.github.com>
2024-10-30 18:21:11 +0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer'
 
 import NextThemeProvider from '@/lib/themeProvider'
 import { inter } from '@/lib/utils'
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import './globals.css'
 
@@ -23,6 +24,7 @@ export default function RootLayout({
             {children}
             <Footer />
           </div>
+          <Analytics />
         </NextThemeProvider>
       </body>
     </html>
